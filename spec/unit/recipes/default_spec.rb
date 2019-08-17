262d0b2bb4efa885808a21747b95fc23c7a5c4b0
Ally Preston <APreston@spartaglobal.com>
2019-08-17 12:17:16 +0100

    reverted back to old version

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,24 +6,29 @@
 
 require 'spec_helper'
 
-describe 'development::default' do
+describe 'python-test::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
-
+  # for a complete list of available platforms and versions see:
+  # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+  platform 'ubuntu', '18.04'
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
-
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should update the sources list' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
+    it 'should install python' do
+      expect(chef_run).to install_package 'python'
+    end
+    it 'should install pip for python' do
+      expect(chef_run).to install_package 'python-pip'
+    end
+    it 'should use pip to install plugins' do
+      expect(chef_run).to run_execute('python-pip install plugins')
+    end
+    it 'should install two plugins for gnureadline' do
+      expect(chef_run).to install_package 'libncurses5-dev'
+      expect(chef_run).to install_package 'libffi-dev'
     end
   end
 end
